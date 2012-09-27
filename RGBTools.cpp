5c044986bfcdd197b690d6e31ae1bf81a98756d8
Johannes Mittendorfer <johannes.mittendorfer@gmail.com>
2012-09-27 18:13:38 +0200

    more comments added
    I added more comments to class to make the code more clear.

--- RGBTools.cpp
+++ RGBTools.cpp
@@ -1,13 +1,16 @@
 #include "RGBTools.h"
 
+// pins for colors
 int r_pin;
 int g_pin;
 int b_pin;
 
+// saves current state (color)
 int curr_r = 0;
 int curr_g = 0;
 int curr_b = 0;
 
+// constructor; saves the pins
 RGBTools::RGBTools(int r, int g, int b){
 	r_pin = r;
 	g_pin = g;
@@ -16,10 +19,12 @@ RGBTools::RGBTools(int r, int g, int b){
 
 // Set LED-color to custom color instantely
 void RGBTools::setColor(int r, int g, int b){
+	// set color of LED
 	analogWrite(r_pin,255-r);
     analogWrite(g_pin,255-g);
     analogWrite(b_pin,255-b);
 	
+	// save state
 	curr_r = r;
 	curr_g = g;
 	curr_b = b;
@@ -27,19 +32,32 @@ void RGBTools::setColor(int r, int g, int b){
 
 // Fade to custom color in specific time in specific steps
 void RGBTools::fadeTo(int r,int g,int b,int steps,int duration){
+	
+	// calculate differance to target
 	float diff_r = r-curr_r;
     float diff_g = g-curr_g;
     float diff_b = b-curr_b;
     
+	// calculate the width of each step
     float steps_r = diff_r / steps;
     float steps_g = diff_g / steps;
     float steps_b = diff_b / steps;
     
+	// loop through the steps
     for(int i = 0; i < steps; i++){
-        this->setColor(r + i*steps_r,  g + i*steps_g, b + i*steps_b);
+        
+		// set color of current step
+		this->setColor(
+			r + i*steps_r,	// red part plus i times the value of one step
+			g + i*steps_g,	// green part plus i times the value of one step	
+			b + i*steps_b	// blue part plus i times the value of one step
+		);
+		
+		// delay until next step
         delay(duration/steps);  
     }
 	
+	// save state
 	curr_r = r;
 	curr_g = g;
 	curr_b = b;
