57800da7983694b24f57a5bb98403694fa06a95d
Johannes Mittendorfer <johannes.mittendorfer@gmail.com>
2012-09-28 10:32:45 +0200

    fixed tabs

--- RGBTools/RGBTools.cpp
+++ RGBTools/RGBTools.cpp
@@ -21,9 +21,9 @@ RGBTools::RGBTools(int r, int g, int b){
 void RGBTools::setColor(int r, int g, int b){
 	// set color of LED
 	analogWrite(r_pin,255-r);
-    analogWrite(g_pin,255-g);
-    analogWrite(b_pin,255-b);
-	
+	analogWrite(g_pin,255-g);
+	analogWrite(b_pin,255-b);
+
 	// save state
 	curr_r = r;
 	curr_g = g;
@@ -32,31 +32,31 @@ void RGBTools::setColor(int r, int g, int b){
 
 // Fade to custom color in specific time in specific steps
 void RGBTools::fadeTo(int r,int g,int b,int steps,int duration){
-	
+
 	// calculate differance to target
 	float diff_r = r-curr_r;
-    float diff_g = g-curr_g;
-    float diff_b = b-curr_b;
-    
+	float diff_g = g-curr_g;
+	float diff_b = b-curr_b;
+
 	// calculate the width of each step
-    float steps_r = diff_r / steps;
-    float steps_g = diff_g / steps;
-    float steps_b = diff_b / steps;
-    
+	float steps_r = diff_r / steps;
+	float steps_g = diff_g / steps;
+	float steps_b = diff_b / steps;
+
 	// loop through the steps
-    for(int i = 0; i < steps; i++){
-        
+	for(int i = 0; i < steps; i++){
+
 		// set color of current step
 		this->setColor(
 			r + i*steps_r,	// red part plus i times the value of one step
 			g + i*steps_g,	// green part plus i times the value of one step	
 			b + i*steps_b	// blue part plus i times the value of one step
 		);
-		
+
 		// delay until next step
-        delay(duration/steps);  
-    }
-	
+		delay(duration/steps);  
+	}
+
 	// save state
 	curr_r = r;
 	curr_g = g;
