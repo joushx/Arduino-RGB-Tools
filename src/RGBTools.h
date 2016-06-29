0fa51cf7688f9ad53d66bf3ef6590b2d0874ba24
Johannes Mittendorfer <johannes@johannes-mittendorfer.com>
2016-06-29 11:47:55 +0200

    Remove color class

--- src/RGBTools.h
+++ src/RGBTools.h
@@ -29,17 +29,9 @@ class RGBTools
 		// saves current state (color)
 		uint8_t curr_r, curr_g, curr_b;
 
-		// output for common cathode RGB leds (0 = dark)
-		// default: common anode (0 = bright)
+		// output mode for common cathode or anode RGB LEDs
 		uint8_t mode;
 
-};
-
-class Color {
-  public:
-    static const uint32_t RED = (const uint32_t) 0xFF0000;
-    static const uint32_t GREEN = (const uint32_t) 0x00FF00;
-    static const uint32_t BLUE = (const uint32_t) 0x0000FF;
 };
 
 #endif
