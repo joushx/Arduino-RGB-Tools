3d60f6549d72e298a8bbb1c0038353418a0690e2
Johannes Mittendorfer <johannes@johannes-mittendorfer.com>
2016-06-29 11:26:22 +0200

    Clean code

--- src/RGBTools.cpp
+++ src/RGBTools.cpp
@@ -7,83 +7,87 @@
 */
 
 #include "RGBTools.h"
-//
+
 // constructor; saves the pins
-RGBTools::RGBTools(int r, int g, int b){
-	this->r_pin = r;
-	this->g_pin = g;
-	this->b_pin = b;
+RGBTools::RGBTools(uint8_t r, uint8_t g, uint8_t b) {
+    this->r_pin = r;
+    this->g_pin = g;
+    this->b_pin = b;
 }
 
 /**
  * Constructor using the Mode enum in header file.
  * Ex: RGBTools(9, 10, 11, COMMON_ANODE);
  */
-RGBTools::RGBTools(int r, int g, int b, Mode mode){
-	this->r_pin = r;
-	this->g_pin = g;
-	this->b_pin = b;
-	if(mode == COMMON_CATHODE) this->_common_cathode = 1;
-	else this->_common_cathode = 0;
+RGBTools::RGBTools(uint8_t r, uint8_t g, uint8_t b, Mode mode) {
+    this->r_pin = r;
+    this->g_pin = g;
+    this->b_pin = b;
+
+    this->mode = mode;
 }
 
-RGBTools::RGBTools(int r, int g, int b, uint8_t common_cathode){
-	this->r_pin = r;
-	this->g_pin = g;
-	this->b_pin = b;
+RGBTools::RGBTools(uint8_t r, uint8_t g, uint8_t b, uint8_t mode) {
+    this->r_pin = r;
+    this->g_pin = g;
+    this->b_pin = b;
 
-	this->_common_cathode = common_cathode;
+    this->mode = mode;
 }
 
 // Set LED-color to custom color instantely
-void RGBTools::setColor(int r, int g, int b){
-	// set color of LED
-	if (_common_cathode) {
-		analogWrite(r_pin, r);
-		analogWrite(g_pin, g);
-		analogWrite(b_pin, b);
-	} else {
-		analogWrite(r_pin, 255 - r);
-		analogWrite(g_pin, 255 - g);
-		analogWrite(b_pin, 255 - b);
-	}
+void RGBTools::setColor(uint8_t r, uint8_t g, uint8_t b) {
+    // set color of LED
+    if (mode == COMMON_CATHODE) {
+        analogWrite(r_pin, r);
+        analogWrite(g_pin, g);
+        analogWrite(b_pin, b);
+    } else {
+        analogWrite(r_pin, 255 - r);
+        analogWrite(g_pin, 255 - g);
+        analogWrite(b_pin, 255 - b);
+    }
 
-	// save state
-	this->curr_r = r;
-	this->curr_g = g;
-	this->curr_b = b;
+    // save state
+    this->curr_r = r;
+    this->curr_g = g;
+    this->curr_b = b;
 }
 
 // Use a 32 bit hex value to set a color. See Colors class in header file
-void RGBTools::setColor(uint32_t color){
-	setColor((color & 0xFF0000) >> 16, (color & 0x00FF00) >> 8, color & 0x0000FF);
+void RGBTools::setColor(uint32_t color) {
+    uint8_t red = (uint8_t) ((color & 0xFF0000) >> 16);
+    uint8_t green = (uint8_t) (color & 0x00FF00) >> 8;
+    uint8_t blue = (uint8_t) (color & 0x0000FF);
+    setColor(red, green, blue);
 }
 
 // Fade to custom color in specific time in specific steps
-void RGBTools::fadeTo(int r,int g,int b,int steps,int duration){
+void RGBTools::fadeTo(uint8_t r, uint8_t g, uint8_t b, int steps, int duration) {
+    // calculate difference to target
+    uint8_t diff_r = r-curr_r;
+    uint8_t diff_g = g-curr_g;
+    uint8_t diff_b = b-curr_b;
 
-	// calculate difference to target
-	int diff_r = r-curr_r;
-	int diff_g = g-curr_g;
-	int diff_b = b-curr_b;
+    // calculate the width of each step multiplied by 100 as to not lose decimal data
+    int steps_r = (diff_r *100 / steps);
+    int steps_g = (diff_g *100 / steps);
+    int steps_b = (diff_b *100 / steps);
 
-	// calculate the width of each step multiplied by 100 as to not lose decimal data
-	int steps_r = (diff_r *100 / steps);
-	int steps_g = (diff_g *100 / steps);
-	int steps_b = (diff_b *100 / steps);
+    // calculate delay between steps
+    int delay_value = (duration / steps);
 
+    // loop through the steps (i: distance to target)
+    for (int i = steps - 1; i >= 0; i--){
 
-	// loop through the steps (i: distance to target)
-	for(int i = steps - 1; i >= 0; i--){
+        // set color of current step
+        this->setColor(
+            r - (i * steps_r)/100,	// red target minus distance to target steps (divided by 100 as to offset steps_r calculations)
+            g - (i * steps_g)/100,	// green
+            b - (i * steps_b)/100 	// blue
+        );
 
-		// set color of current step
-		this->setColor(
-			r - (i * steps_r)/100,	// red target minus distance to target steps (divided by 100 as to offset steps_r calculations)
-			g - (i * steps_g)/100,	// green
-			b - (i * steps_b)/100 	// blue
-		);
-
-		// delay until next step
-		delay(duration/steps);  
-	}
+        // delay until next step
+        delay((unsigned long) delay_value);
+    }
 }
--- src/RGBTools.h
+++ src/RGBTools.h
@@ -16,31 +16,31 @@ enum Mode { COMMON_ANODE, COMMON_CATHODE };
 class RGBTools
 {
 	public:
-		RGBTools(int r, int g, int b);
-		RGBTools(int r, int g, int b, uint8_t common_cathode);
-		RGBTools(int r, int g, int b, Mode mode);
-		void setColor(int r, int g, int b);
+		RGBTools(uint8_t r, uint8_t g, uint8_t b);
+		RGBTools(uint8_t r, uint8_t g, uint8_t b, uint8_t mode);
+		RGBTools(uint8_t r, uint8_t g, uint8_t b, Mode mode);
+		void setColor(uint8_t r, uint8_t g, uint8_t b);
 		void setColor(uint32_t);
-		void fadeTo(int r,int g,int b,int steps,int duration);
+		void fadeTo(uint8_t r, uint8_t g, uint8_t b, int steps, int duration);
 		
 	private:
 		// pins for colors
-		int r_pin, g_pin, b_pin;
+		uint8_t r_pin, g_pin, b_pin;
 
 		// saves current state (color)
-		int curr_r, curr_g, curr_b;
+		uint8_t curr_r, curr_g, curr_b;
 
 		// output for common cathode RGB leds (0 = dark)
 		// default: common anode (0 = bright)
-		uint8_t _common_cathode;
+		uint8_t mode;
 
 };
 
 class Color {
   public:
-    static const uint32_t RED = 0xFF0000;
-    static const uint32_t GREEN = 0x00FF00;
-    static const uint32_t BLUE = 0x0000FF;
+    static const uint32_t RED = (const uint32_t) 0xFF0000;
+    static const uint32_t GREEN = (const uint32_t) 0x00FF00;
+    static const uint32_t BLUE = (const uint32_t) 0x0000FF;
 };
 
 #endif
